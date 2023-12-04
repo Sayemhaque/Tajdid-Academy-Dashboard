756a6f67c535c0e743ddd09ddfe26537b13ccd53
Mohammad sayem Haque <85791327+Sayemhaque@users.noreply.github.com>
2023-12-04 07:08:51 +0600

    refactor: Create SidebarLink component for DRY code

--- src/components/Sidebar/SidebarLinks.jsx
+++ src/components/Sidebar/SidebarLinks.jsx
@@ -1,31 +1,32 @@
-import { CiBoxes, CiDollar, CiLogout,CiUser, CiWavePulse1 } from "react-icons/ci";
+/* eslint-disable react/prop-types */
+import { CiBoxes, CiDollar, CiHome, CiLogout, CiUser, CiWavePulse1 } from "react-icons/ci";
+import { FiChevronDown } from "react-icons/fi";
+
+const SidebarLink = ({ icon, text,ChevronDownIcon }) => (
+  <li className="flex justify-between items-center text-md cursor-pointer px-8 text-[#101828] py-2 font-semibold">
+    <span className="flex gap-2">
+      {icon}
+      {text}
+    </span>
+    {ChevronDownIcon}
+  </li>
+);
+
 const SidebarLinks = () => {
-    return (
-        <nav className="mt-12">
-            <ul className="space-y-3">
-                <li className="bg-[#EFF8FF] text-md cursor-pointer px-8 text-[#101828] py-2 font-semibold flex items-center gap-2">
-                    <CiBoxes className="text-2xl" />
-                    Products
-                </li>
-                <li className="text-md cursor-pointer px-8 text-[#101828] py-2 font-semibold flex items-center gap-2">
-                    <CiWavePulse1 className="text-2xl" />
-                    Analatics
-                </li>
-                <li className="text-md cursor-pointer px-8 text-[#101828] py-2 font-semibold flex items-center gap-2">
-                    <CiDollar className="text-2xl" />
-                    Billing
-                </li>
-                <li className="text-md cursor-pointer px-8 text-[#101828] py-2 font-semibold flex items-center gap-2">
-                    <CiUser className="text-2xl" />
-                    Admin
-                </li>
-                <li className="text-md cursor-pointer px-8 text-[#101828] py-2 font-semibold flex items-center gap-2">
-                    <CiLogout className="text-2xl" />
-                    Logout
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="mt-12">
+      <ul className="space-y-3">
+        <SidebarLink icon={<CiBoxes className="text-2xl" />} text="Products" />
+        <SidebarLink icon={<CiWavePulse1 className="text-2xl" />} text="Analytics"        ChevronDownIcon={<FiChevronDown/>}/>
+        <SidebarLink icon={<CiDollar className="text-2xl" />} text="Billing" 
+         ChevronDownIcon={<FiChevronDown/>}/>
+        <SidebarLink icon={<CiHome className="text-2xl" />} text="Company" 
+         ChevronDownIcon={<FiChevronDown/>}/>
+        <SidebarLink icon={<CiUser className="text-2xl" />} text="Admin" />
+        <SidebarLink icon={<CiLogout className="text-2xl" />} text="Logout" />
+      </ul>
+    </nav>
+  );
 };
 
 export default SidebarLinks;
