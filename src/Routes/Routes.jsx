a2cdde9711834bc36357860f46607d2c1ae10b99
Mohammad sayem Haque <85791327+Sayemhaque@users.noreply.github.com>
2023-12-04 14:56:18 +0600

    chore: set up tanstack query

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App"
-import Product from "../components/Products/Product";
 import Analytics from "../components/Analytics/Analytics";
 import Billing from "../components/Billing/Billing";
 import Company from "../components/Company/Company";
+import ProductContent from "../components/Products/ProductContent";
 
 export const router = createBrowserRouter([
     {
@@ -13,7 +13,7 @@ export const router = createBrowserRouter([
       children:[
         {
             path:"/",
-            element:<Product/>
+            element:<ProductContent/>
         },
         {
             path:"/analytics",
---
+++ src/components/Products/ProductContent.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+const ProductContent = () => {
+    return (
+        <>
+          <Header/>
+        </>
+    );
+};
+
+export default ProductContent;
